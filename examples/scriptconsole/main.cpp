f4252a4f7e93c7d9c686cbc542904db40e0c3c0d
George Apostolopoulos <gapost@ipta.demokritos.gr>
2020-09-24 12:16:32 +0300

    Use a monospace font in the example.

--- examples/scriptconsole/main.cpp
+++ examples/scriptconsole/main.cpp
@@ -1,7 +1,6 @@
 #include "QConsoleWidget.h"
 #include "scriptobject.h"
 #include <QApplication>
-#include <QTimer>
 
 
 int main(int argc, char *argv[])
@@ -12,14 +11,21 @@ int main(int argc, char *argv[])
     ScriptObject qs(&w);
 
     w.setWindowTitle("qscript console");
+#if defined(Q_OS_MAC)
+    w.setFont(QFont("Monaco"));
+#elif defined(Q_OS_UNIX)
+    w.setFont(QFont("Monospace"));
+#elif defined(Q_OS_WIN)
+    w.setFont(QFont("Courier New"));
+#endif
     w.writeStdOut(
-                "QConsoleWidget example: "
-                "  interactive qscript interpreter\n\n"
+                "QConsoleWidget example:"
+                " interactive qscript interpreter\n\n"
                 "Additional commands:\n"
-                "  - quit()  : end program\n"
-                "  - exit()  : same\n"
-                "  - log(x)  : printout x.toString()\n"
-                "  - wait(ms): block qscript execution for given ms\n\n"
+                "  - quit()   : end program\n"
+                "  - exit()   : same\n"
+                "  - log(x)   : printout x.toString()\n"
+                "  - wait(ms) : block qscript execution for given ms\n\n"
                 );
     w.writeStdOut("qs> ");
     w.setMode(QConsoleWidget::Input);
